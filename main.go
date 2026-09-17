46f78eaf1ff63d566459427ed35ddf24e839e009
Mark Bryan De Guzman <mdeguzman.cpe@gmail.com>
2026-09-18 00:04:56 +0800

    Move deployment type to env

--- main.go
+++ main.go
@@ -21,23 +21,25 @@ func main() {
 		return
 	}
 
-	dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
-	if err != nil {
-		dbPort = 5432
-	}
-
 	appPort, err := strconv.Atoi(os.Getenv("UADMIN_PORT"))
 	if err != nil {
 		appPort = 1123
 	}
 
-	uadmin.Database = &uadmin.DBSettings{
-		Host:     os.Getenv("DB_HOST"),
-		Name:     os.Getenv("DB_NAME"),
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		Port:     dbPort,
-		Type:     "postgres",
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION" {
+		dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
+		if err != nil {
+			dbPort = 5432
+		}
+
+		uadmin.Database = &uadmin.DBSettings{
+			Host:     os.Getenv("DB_HOST"),
+			Name:     os.Getenv("DB_NAME"),
+			User:     os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Port:     dbPort,
+			Type:     "postgres",
+		}
 	}
 
 	uadmin.Register(
